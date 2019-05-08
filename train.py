a4e082c2b861ed5345364596ed58014001200ea2
antoine_rbn <a.robinwood@live.fr>
2019-05-08 10:02:33 +0200

    Add iamondb reader

--- train.py
+++ train.py
@@ -6,31 +6,97 @@ from lstm_vae import create_lstm_vae, inference
 import keras
 import matplotlib.pyplot as plt
 
+dataset = 'iamondb'
 
-def get_text_data(num_samples=1000):
+def load_sequences(source_dir, seq_file='sequences.npy', idx_file='sequence_indices.npy'):
+    """ load data mat and length max, add lengths up to get indices """
+    seq_mat = np.load(source_dir + '/' + seq_file)
+    idx_mat = np.load(source_dir + '/' + idx_file)
+    # plt.hist(idx_mat, 50, normed=1, facecolor='green', alpha=0.75)
+    # plt.show()
+    for idx in range(1, idx_mat.shape[0]):
+        idx_mat[idx] = idx_mat[idx] + idx_mat[idx - 1]
+
+    return seq_mat, idx_mat
+
+def load_and_cut_sequences(source_dir, seq_file='train_sequences.npy', idx_file='train_sequence_indices.npy', cut_len=500,
+                           normalize=True, mask=True, mask_value=500):
+    """ loads sequences, cuts to certain lenght. optionally normalizing and masking them """
+    if not mask:
+        mask_value = 0
+
+    seq_mat, idx_mat = load_sequences(source_dir, seq_file, idx_file)
+
+    split_list = np.split(seq_mat.astype(float), idx_mat[1:], axis=0)
+
+    # cut sequences to maximum length
+    cut_list = []
+    for mat in split_list:
+        if mat.shape[0] > cut_len:
+            cut_list.append(mat[:cut_len, :])
+        else:
+            cut_list.append(mat)
+
+    cut_seq_mat = np.concatenate(cut_list, axis=0)
+    
+    # compute adequate mean and std-dev
+    if normalize:
+        if mask:
+            cut_mat = cut_seq_mat.astype(float)
+        else:  # append as many zeros, as will be padded to cut_seq_mat
+            zero_shape = (len(cut_list) * cut_len - cut_seq_mat.shape[0], cut_seq_mat.shape[1])
+            cut_mat = np.concatenate([cut_seq_mat.astype(float), np.zeros(zero_shape, dtype=float)], axis=0)
+
+        mean = np.mean(cut_mat, axis=0)
+        std = np.std(cut_mat, axis=0)
+        # for idx in [0, 1]:
+        #     mat[:, idx] = (mat[:, idx] - mean[idx]) / std[idx]
+
+    else:
+        mean = [0., 0., 0.]
+        std = [1., 1., 1.]
+
+    # normalize and pad sequences
+    for idx, mat in enumerate(cut_list):
+        if normalize:
+            mat[:, 0] = (mat[:, 0] - mean[0]) / std[0]
+            mat[:, 1] = (mat[:, 1] - mean[1]) / std[1]
+
+        if mat.shape[0] < cut_len:
+            padded = np.zeros((cut_len, 3), dtype=float) + mask_value
+            padded[:mat.shape[0], :] = mat
+            mat = padded
+
+        cut_list[idx] = mat
+
+    data_mat = np.asarray(cut_list)
+    
+    return data_mat, mean, std
+
+
+def get_dataset(num_samples=1000, data_type = 'mnist'):
             
     # load data
-    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
-    n_inputs, height, max_length = X_train.shape
-    encoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
-    decoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
-    encoder_input_data[:,:max_length,:] = np.swapaxes(X_train, 1, 2).copy()/255
-    decoder_input_data[:,1:,:] = np.swapaxes(X_train, 1, 2).copy()/255
+    if data_type == 'mnist':
+        (X_train, _), _ = keras.datasets.mnist.load_data()
+        n_inputs, height, max_length = X_train.shape
+        encoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
+        decoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
+        encoder_input_data[:,:max_length,:] = np.swapaxes(X_train, 1, 2).copy()/255
+        decoder_input_data[:,1:,:] = np.swapaxes(X_train, 1, 2).copy()/255
+        
+    elif data_type == 'iamondb':
+        data_mat, mean, stp = load_and_cut_sequences(source_dir = 'data/handwriting', mask = False, cut_len = 2000, normalize = False)
+        n_inputs, height, max_length = data_mat.shape
+        encoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
+        decoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
 
     return max_length + 1, height, encoder_input_data[:num_samples,:,:], decoder_input_data[:num_samples,:,:]
 
 
 if __name__ == "__main__":
-    
-    # load data
-    (X_train, y_train), (X_test, y_test) = keras.datasets.mnist.load_data()
-    n_inputs, height, max_length = X_train.shape
-    encoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
-    decoder_input_data = np.zeros((n_inputs, max_length + 1, height), dtype="float32")
-    encoder_input_data[:,:max_length,:] = np.swapaxes(X_train, 1, 2).copy()/255
-    decoder_input_data[:,1:,:] = np.swapaxes(X_train, 1, 2).copy()/255
 
-    timesteps_max, enc_tokens, x, x_decoder = get_text_data(num_samples=3000)
+    timesteps_max, enc_tokens, x, x_decoder = get_dataset(num_samples=3000, data_type = dataset)
 
     print(x.shape, "Creating model...")
 
